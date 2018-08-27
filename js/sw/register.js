7ac5050b040370c92413a9b6e7c23199daff75d2
Benjamin MArtinez <benmar000@gmail.com>
2018-08-27 19:28:16 -0400

    add meria queries for mobile

--- js/sw/register.js
+++ js/sw/register.js
@@ -1,15 +1,11 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            //registration was successful 
+            // registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
-            //registration failed 
+            // registration failed
             console.log('Registration unsuccesful with error: ', err);
         });
     });
 }
-
-
-
-
--- sw.js
+++ sw.js
@@ -1,69 +1,70 @@
 var CACHE_NAME = 'mws-cache-v1';
 var urlsToCache = [
-    '/',
-    '/css/styles.css',
-    '/js/',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
-    '/js/sw/register.js',
-    '/index.html',
-    '/restaurant.html',
-    '/data/restaurants.json',
-];
+  '/',
+  '/css/styles.css',
+  '/js/',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+  '/js/sw/register.js',
+  '/index.html',
+  '/restaurant.html',
+  '/data/restaurants.json'
+]
 
 self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            }).catch(function (error) {
-                console.log('Failed to open cache with error: ', error)
-            })
-    );
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      }).catch(function (error) {
+        console.log('Failed to open cache with error: ', error)
+      })
+  );
 });
 
 self.addEventListener('fetch', function (event) {
-    //Perform fetch steps 
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                //Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                //Cache not hit 
+  // Perform fetch steps
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Cache not hit
 
-                //Clone the request since it's a stream that can only be used once. 
-                var fetchRequest = event.request.clone();
+        // Clone the request since it's a stream that can only be used once. 
+        var fetchRequest = event.request.clone();
 
-                return fetch(fetchRequest)
-                    .then(function (response) {
-                        // check if response is valid
-                        // response type 'basic' prevents third party resources from saving
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+        return fetch (fetchRequest)
+          .then(function (response) {
+            // check if response is valid
+            // response type 'basic' prevents third party resources from saving
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-                        //Clone the response since it's a stream that can only be used once. 
-                        var fetchResponse = response.clone();
+            // Clone the response since it's a stream that can only be used once
+            var fetchResponse = response.clone();
 
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                console.log("saving to cache");
-                                cache.put(event.request, fetchResponse);
-                            });
-                        return response;
-                    }).catch(function (error) {
-                        return new Response("Application is not connected to the internet", {
-                            status: 404,
-                            statusText: "Application is not connected to the internet"
-                        });
-                    })
-            })
-    )
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                console.log('saving to cache');
+                cache.put(event.request, fetchResponse);
+              });
+            return response;
+          }).catch(function (error) {
+            console.log(error);
+            return new Response('Application is not connected to the internet', {
+              status: 404,
+              statusText: 'Application is not connected to the internet',
+            });
+          })
+      })
+  )
 });
 
 
